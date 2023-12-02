0baf81b3be8e643c3784cee3563931b4ef5f9258
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-01 22:08:48 -0300

    Criado tela base de cadastro de comunidade - 2023.12.1.5

---
+++ lib/pages/comunidade/cadastro_comunidade.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CadastroComunidade extends StatefulWidget {
+  const CadastroComunidade({super.key});
+
+  @override
+  State<CadastroComunidade> createState() => _CadastroComunidadeState();
+}
+
+class _CadastroComunidadeState extends State<CadastroComunidade> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
