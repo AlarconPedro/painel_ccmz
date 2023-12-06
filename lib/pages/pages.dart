30603fa4db371b3ad5044d8a6ea66a03ba3ada15
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-05 22:01:49 -0300

    Criado cadastro de blocos - 2023.12.5.5

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -4,6 +4,7 @@ export 'pessoas/pessoas.dart';
 export 'pessoas/cadastro_pessoas.dart';
 
 export 'bloco/bloco.dart';
+export 'bloco/cadastro_bloco.dart';
 
 export 'comunidade/comunidade.dart';
 export 'comunidade/cadastro_comunidade.dart';
