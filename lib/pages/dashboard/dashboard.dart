5416ace8fc9bbf6fda434298b655f05ece8c8962
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-08 18:07:34 -0300

    Iniciando design da tela de Dashboard - 2023.12.8.14

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../classes/classes.dart';
+
 class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
 
@@ -10,6 +12,76 @@ class DashBoard extends StatefulWidget {
 class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Cores.cinzaClaro,
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 150,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  elevation: 10,
+                  color: Cores.branco,
+                  child: Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Cores.branco,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 10,
+                        color: Cores.branco,
+                        child: Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Cores.branco,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 250,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 10,
+                        color: Cores.branco,
+                        child: Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Cores.branco,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
