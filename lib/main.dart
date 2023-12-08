1200101727918d717455b58f1aecb6e118ec1dad
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-08 15:27:00 -0300

    Adicionado compatibilidade com linaguagem pt-br - 2023.12.8.9

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:painel_ccmz/estrutura/estrutura_page.dart';
 import 'package:painel_ccmz/pages/login/login_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      localizationsDelegates: const [GlobalMaterialLocalizations.delegate],
       debugShowCheckedModeBanner: false,
+      locale: const Locale('pt', 'BR'),
       title: 'Painel CCMZ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
