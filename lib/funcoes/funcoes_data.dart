caada19bed4e52bdac222e16d0b33746d7992066
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-08 15:26:36 -0300

    Adicionado função de conversão de data para Upload - 2023.12.8.8

--- lib/funcoes/funcoes_data.dart
+++ lib/funcoes/funcoes_data.dart
@@ -10,7 +10,8 @@ class FuncoesData {
     if (data == null || data.isEmpty) {
       return '';
     }
-    return '${data.substring(8, 10)}/${data.substring(5, 7)}/${data.substring(0, 4)} ${data.substring(11, 13)}:${data.substring(14, 16)}';
+    // return '${data.substring(8, 10)}/${data.substring(5, 7)}/${data.substring(0, 4)} ${data.substring(11, 13)}:${data.substring(14, 16)}';
+    return '${data.substring(8, 10)}/${data.substring(5, 7)}/${data.substring(0, 4)}';
   }
 
   static String dataFormatadaHoraSegundo(String data) {
@@ -38,5 +39,12 @@ class FuncoesData {
         data.substring(14, 16) +
         data.substring(17, 19) +
         data.substring(20, 23);
+  }
+
+  static stringToDateTime(String data) {
+    List<String> dataSplit = data.split('/');
+    String dataFormatada =
+        "${dataSplit[2]}-${dataSplit[1]}-${dataSplit[0]}T00:00:00";
+    return dataFormatada;
   }
 }
