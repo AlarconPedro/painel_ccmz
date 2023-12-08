92b16346a1dcf3605a5dcc376291c50d91919548
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-08 11:36:39 -0300

    Criado Model Evento - 2023.12.8.2

---
+++ lib/data/models/evento_model.dart
@@ -0,0 +1,30 @@
+import 'package:painel_ccmz/funcoes/funcoes.dart';
+
+class EventoModel {
+  int eveCodigo;
+  String eveNome;
+  String eveDataInicio;
+  String eveDataFim;
+
+  EventoModel({
+    required this.eveCodigo,
+    required this.eveNome,
+    required this.eveDataInicio,
+    required this.eveDataFim,
+  });
+
+  factory EventoModel.fromJson(Map<String, dynamic> json) => EventoModel(
+        eveCodigo: json["eveCodigo"] ?? 0,
+        eveNome: json["eveNome"] ?? "",
+        eveDataInicio:
+            FuncoesData.dataFormatadaHora(json["eveDatainicio"] ?? ""),
+        eveDataFim: FuncoesData.dataFormatadaHora(json["eveDatafim"] ?? ""),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "eveCodigo": eveCodigo,
+        "eveNome": eveNome,
+        "eveDataInicio": eveDataInicio,
+        "eveDataFim": eveDataFim,
+      };
+}
