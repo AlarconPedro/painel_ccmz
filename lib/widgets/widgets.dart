127a5031da9f5080092bcdeb67ce3f1f8fc330a4
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-09 11:24:57 -0300

    Adicionado física de rolagem ao projeto - 2023.12.9.1

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -3,5 +3,8 @@ export 'cards/card_comunidade.dart';
 export 'cards/card_bloco.dart';
 export 'cards/card_quartos.dart';
 export 'cards/card_evento.dart';
+export 'cards/card_corpo_tela.dart';
 
 export 'loading/carregamento_ios.dart';
+
+export 'scrol/scroll_list.dart';
