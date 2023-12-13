f143efb51e32b61fa0de501e2112a77f411b70c4
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-12 23:48:30 -0300

    Adicionado novo formato de upload para lista de dados - 2023.12.12.5

--- lib/data/http/http_request.dart
+++ lib/data/http/http_request.dart
@@ -17,6 +17,16 @@ class HttpRequest {
     );
   }
 
+  Future<http.Response> postListJson(String url, List<dynamic> body) async {
+    return await http.post(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode(body),
+    );
+  }
+
   Future<http.Response> putJson(String url, Map<String, dynamic> body) async {
     return await http.put(
       Uri.parse(url),
