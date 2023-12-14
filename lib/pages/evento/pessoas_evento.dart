8ccfd9cf2b7f170d273f530418fd47243505b234
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-13 21:46:17 -0300

    Adicionado modelo base da tela de pessoas - 2023.12.13.2

--- lib/pages/evento/pessoas_evento.dart
+++ lib/pages/evento/pessoas_evento.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../classes/classes.dart';
 
 class PessoasEvento extends StatefulWidget {
   const PessoasEvento({super.key});
@@ -10,6 +13,29 @@ class PessoasEvento extends StatefulWidget {
 class _PessoasEventoState extends State<PessoasEvento> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Card(
+            color: Cores.branco,
+            elevation: 10,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 1.1,
+              width: MediaQuery.of(context).size.width / 1.5,
+              child: const Column(
+                children: [],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
