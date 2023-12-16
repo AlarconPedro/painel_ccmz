30487a4835fc9528f3dbee0da97f34cead293486
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-16 20:08:01 -0300

    Subido pra o servidor - 2023.16.12.1

--- lib/widgets/cards/mostragem/card_pessoas_evento.dart
+++ lib/widgets/cards/mostragem/card_pessoas_evento.dart
@@ -40,37 +40,58 @@ class _CardPessoasEventoState extends State<CardPessoasEvento> {
           borderRadius: BorderRadius.circular(5),
           color: widget.selecionado ? Cores.azulClaro : Cores.branco,
         ),
-        child: Row(children: [
-          Container(
-            color: Cores.cinzaClaro,
-            width: 100,
-            child: Center(
-              child: Icon(
-                widget.pessoa.pesGenero == "F"
-                    ? Icons.female_rounded
-                    : Icons.male_rounded,
-                size: 50,
+        child: Row(
+          children: [
+            Container(
+              color: Cores.cinzaClaro,
+              width: 100,
+              child: Center(
+                child: Icon(
+                  widget.pessoa.pesGenero == "F"
+                      ? Icons.female_rounded
+                      : Icons.male_rounded,
+                  size: 50,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-                color: widget.selecionado ? Cores.azulClaro : Cores.branco,
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              child: Text(
-                                textAlign: TextAlign.center,
-                                overflow: TextOverflow.ellipsis,
-                                widget.pessoa.pesNome,
+            Expanded(
+              flex: 3,
+              child: Container(
+                  color: widget.selecionado ? Cores.azulClaro : Cores.branco,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  textAlign: TextAlign.center,
+                                  overflow: TextOverflow.ellipsis,
+                                  widget.pessoa.pesNome,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: widget.selecionado
+                                        ? Cores.branco
+                                        : Cores.cinzaEscuro,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Comunidade:",
                                 style: TextStyle(
                                   fontSize: 16,
                                   fontWeight: FontWeight.bold,
@@ -79,133 +100,114 @@ class _CardPessoasEventoState extends State<CardPessoasEvento> {
                                       : Cores.cinzaEscuro,
                                 ),
                               ),
-                            )
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Comunidade:",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: widget.selecionado
-                                    ? Cores.branco
-                                    : Cores.cinzaEscuro,
-                              ),
-                            ),
-                            Text(
-                              widget.pessoa.comunidade,
-                              style: TextStyle(
-                                color: widget.selecionado
-                                    ? Cores.branco
-                                    : Cores.cinzaEscuro,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Sexo:",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: widget.selecionado
-                                    ? Cores.branco
-                                    : Cores.cinzaEscuro,
-                              ),
-                            ),
-                            Text(
-                              widget.pessoa.pesGenero,
-                              style: TextStyle(
-                                color: widget.selecionado
-                                    ? Cores.branco
-                                    : Cores.cinzaEscuro,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          children: [
-                            Text(
-                              "Pagante:",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: widget.selecionado
-                                    ? Cores.branco
-                                    : Cores.cinzaEscuro,
-                              ),
-                            ),
-                            CupertinoCheckbox(
-                              value: pagante,
-                              onChanged: (value) {
-                                setState(() {
-                                  pagante = value!;
-                                });
-                              },
-                              checkColor: widget.selecionado
-                                  ? Cores.branco
-                                  : Cores.cinzaEscuro,
-                              activeColor: widget.selecionado
-                                  ? Cores.cinzaEscuro
-                                  : Cores.branco,
-                              side: BorderSide(
+                              Text(
+                                widget.pessoa.comunidade,
+                                style: TextStyle(
                                   color: widget.selecionado
                                       ? Cores.branco
-                                      : Cores.cinzaEscuro),
-                            ),
-                            const Expanded(child: SizedBox()),
-                            Text(
-                              "Cobrar:",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: widget.selecionado
-                                    ? Cores.branco
-                                    : Cores.cinzaEscuro,
+                                      : Cores.cinzaEscuro,
+                                ),
                               ),
-                            ),
-                            CupertinoCheckbox(
-                              value: cobrar,
-                              onChanged: (value) {
-                                setState(() {
-                                  cobrar = value!;
-                                });
-                              },
-                              checkColor: widget.selecionado
-                                  ? Cores.branco
-                                  : Cores.cinzaEscuro,
-                              activeColor: widget.selecionado
-                                  ? Cores.cinzaEscuro
-                                  : Cores.branco,
-                              side: BorderSide(
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Sexo:",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
                                   color: widget.selecionado
                                       ? Cores.branco
-                                      : Cores.cinzaEscuro),
-                            ),
-                          ],
+                                      : Cores.cinzaEscuro,
+                                ),
+                              ),
+                              Text(
+                                widget.pessoa.pesGenero,
+                                style: TextStyle(
+                                  color: widget.selecionado
+                                      ? Cores.branco
+                                      : Cores.cinzaEscuro,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                )),
-          ),
-        ]),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Pagante:",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: widget.selecionado
+                                      ? Cores.branco
+                                      : Cores.cinzaEscuro,
+                                ),
+                              ),
+                              CupertinoCheckbox(
+                                value: pagante,
+                                onChanged: (value) {
+                                  setState(() {
+                                    pagante = value!;
+                                  });
+                                },
+                                checkColor: widget.selecionado
+                                    ? Cores.branco
+                                    : Cores.cinzaEscuro,
+                                activeColor: widget.selecionado
+                                    ? Cores.cinzaEscuro
+                                    : Cores.branco,
+                                side: BorderSide(
+                                    color: widget.selecionado
+                                        ? Cores.branco
+                                        : Cores.cinzaEscuro),
+                              ),
+                              const Expanded(child: SizedBox()),
+                              Text(
+                                "Cobrar:",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: widget.selecionado
+                                      ? Cores.branco
+                                      : Cores.cinzaEscuro,
+                                ),
+                              ),
+                              CupertinoCheckbox(
+                                value: cobrar,
+                                onChanged: (value) {
+                                  setState(() {
+                                    cobrar = value!;
+                                  });
+                                },
+                                checkColor: widget.selecionado
+                                    ? Cores.branco
+                                    : Cores.cinzaEscuro,
+                                activeColor: widget.selecionado
+                                    ? Cores.cinzaEscuro
+                                    : Cores.branco,
+                                side: BorderSide(
+                                    color: widget.selecionado
+                                        ? Cores.branco
+                                        : Cores.cinzaEscuro),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
