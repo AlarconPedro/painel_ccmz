c3cf95c554294d7bf41baba3a12cb19d5c02d28e
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-12-17 15:14:59 -0300

    Alterado para exibir itens do dashboard - 2023.12.17.2

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -22,89 +22,100 @@ class _DashBoardState extends State<DashBoard> {
           child: Column(
             children: [
               SizedBox(
-                height: 200,
+                height: 120,
                 width: double.infinity,
                 child: CardCorpoTela(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 5,
-                            ),
-                            child: Text(
-                              "Eventos",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
+                  child: const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
                       ),
-                      Expanded(
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          physics: const BouncingScrollPhysics(),
-                          itemCount: 20,
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            return CardCorpoTela(
-                              child: SizedBox(
-                                width: 150,
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          color: Cores.cinzaClaro,
-                                        ),
-                                        child: const Center(
-                                          child: Text(
-                                            "Imagem",
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.all(5),
-                                      child: Text(
-                                        "Nome do Evento",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.all(5),
-                                      child: Text(
-                                        "Data do Evento",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
+                      child: Row(
+                        children: [CardDashboard()],
                       ),
-                    ],
+                    ),
                   ),
+                  //   child: Column(
+                  //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //     children: [
+                  //       const Row(
+                  //         mainAxisAlignment: MainAxisAlignment.start,
+                  //         children: [
+                  //           Padding(
+                  //             padding: EdgeInsets.symmetric(
+                  //               horizontal: 10,
+                  //               vertical: 5,
+                  //             ),
+                  //             child: Text(
+                  //               "Eventos",
+                  //               style: TextStyle(
+                  //                 fontSize: 18,
+                  //                 fontWeight: FontWeight.bold,
+                  //               ),
+                  //             ),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //       Expanded(
+                  //         child: ListView.builder(
+                  //           scrollDirection: Axis.horizontal,
+                  //           physics: const BouncingScrollPhysics(),
+                  //           itemCount: 20,
+                  //           shrinkWrap: true,
+                  //           itemBuilder: (context, index) {
+                  //             return CardCorpoTela(
+                  //               child: SizedBox(
+                  //                 width: 150,
+                  //                 child: Column(
+                  //                   children: [
+                  //                     Expanded(
+                  //                       child: Container(
+                  //                         decoration: BoxDecoration(
+                  //                           borderRadius:
+                  //                               BorderRadius.circular(10),
+                  //                           color: Cores.cinzaClaro,
+                  //                         ),
+                  //                         child: const Center(
+                  //                           child: Text(
+                  //                             "Imagem",
+                  //                             style: TextStyle(
+                  //                               fontSize: 18,
+                  //                               fontWeight: FontWeight.bold,
+                  //                             ),
+                  //                           ),
+                  //                         ),
+                  //                       ),
+                  //                     ),
+                  //                     const Padding(
+                  //                       padding: EdgeInsets.all(5),
+                  //                       child: Text(
+                  //                         "Nome do Evento",
+                  //                         style: TextStyle(
+                  //                           fontSize: 18,
+                  //                           fontWeight: FontWeight.bold,
+                  //                         ),
+                  //                       ),
+                  //                     ),
+                  //                     const Padding(
+                  //                       padding: EdgeInsets.all(5),
+                  //                       child: Text(
+                  //                         "Data do Evento",
+                  //                         style: TextStyle(
+                  //                           fontSize: 18,
+                  //                           fontWeight: FontWeight.bold,
+                  //                         ),
+                  //                       ),
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //               ),
+                  // );
+                  // },
+                  // ),
+                  // ),
+                  // ],
+                  // ),
                 ),
               ),
               Expanded(
