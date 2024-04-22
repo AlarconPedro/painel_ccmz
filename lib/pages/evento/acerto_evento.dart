a25bab9744034e4d9774a15ee4da3efd98d71157
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2024-04-22 20:41:05 -0300

    Alterado estilo de tela para comportar blocos de cobrança separados de acordo com a comunidade - 2024.4.22.1

--- lib/pages/evento/acerto_evento.dart
+++ lib/pages/evento/acerto_evento.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 import '../../classes/classes.dart';
 
@@ -31,170 +33,235 @@ class _AcertoEventoState extends State<AcertoEvento> {
               width: MediaQuery.of(context).size.width / 1.5,
               child: Column(
                 children: [
+                  Card(
+                    elevation: 5,
+                    color: Cores.branco,
+                    shape: const RoundedRectangleBorder(
+                      // borderRadius: BorderRadius.only(
+                      //   topLeft: Radius.circular(10),
+                      //   topRight: Radius.circular(10),
+                      // ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      height: 200,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Evento: Nome do Evento",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  "Cobrante: 80",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  "Pagante: 80",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Text(
+                                  "Valor Cozinha:",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(width: 10),
+                                Expanded(
+                                  child: SizedBox(
+                                    height: 45,
+                                    child: CupertinoTextField(
+                                      placeholder: "R\$ 0,00",
+                                      keyboardType: TextInputType.number,
+                                      inputFormatters: [
+                                        TextInputFormatter.withFunction(
+                                          (oldValue, newValue) {
+                                            if (newValue.text.isEmpty) {
+                                              return newValue.copyWith(
+                                                text: '0,00',
+                                              );
+                                            } else {
+                                              final double value = double.parse(
+                                                newValue.text
+                                                    .replaceAll(',', '.'),
+                                              );
+                                              final String newText =
+                                                  NumberFormat.currency(
+                                                locale: 'pt_BR',
+                                                symbol: 'R\$',
+                                                decimalDigits: 2,
+                                              ).format(value / 100);
+                                              return newValue.copyWith(
+                                                text: newText,
+                                              );
+                                            }
+                                          },
+                                        ),
+                                      ],
+                                      padding: const EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                        // color: Cores.cinzaClaro,
+                                        border: Border.all(
+                                          color: Cores.cinzaEscuro,
+                                          width: 1,
+                                        ),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                const Text(
+                                  "Valor Hostiária:",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(width: 10),
+                                Expanded(
+                                  child: SizedBox(
+                                    height: 45,
+                                    child: CupertinoTextField(
+                                      placeholder: "R\$ 0,00",
+                                      keyboardType: TextInputType.number,
+                                      padding: const EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                        // color: Cores.cinzaClaro,
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
+                                          color: Cores.cinzaEscuro,
+                                          width: 1,
+                                        ),
+                                      ),
+                                      // decoration: BoxDecoration(
+                                      //   color: Cores.cinzaClaro,
+                                      //   borderRadius: BorderRadius.circular(10),
+                                      // ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                const Text(
+                                  "Valor Total:",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(width: 10),
+                                Expanded(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Cores.branco,
+                                      borderRadius: BorderRadius.circular(10),
+                                      border: Border.all(
+                                        color: Cores.cinzaEscuro,
+                                        width: 1,
+                                      ),
+                                    ),
+                                    height: 45,
+                                    child: const Center(
+                                        child: Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 10),
+                                      child: Row(
+                                        children: [
+                                          Text("R\$ 0,00"),
+                                        ],
+                                      ),
+                                    )),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                   const SizedBox(height: 10),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      children: [
-                        Center(
-                          child: Text(
-                            "Acerto do Evento",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Spacer(),
-                        Text("Cobrantes: 80",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                        SizedBox(width: 10),
-                        Text("Pagantes: 80",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                      ],
+                  Card(
+                    elevation: 5,
+                    color: Cores.branco,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
                     ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 15,
-                      horizontal: 10,
-                    ),
-                    child: Divider(
-                      color: Cores.cinzaEscuro,
-                      thickness: 1,
-                    ),
-                  ),
-                  const Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Evento:",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                      ),
-                      Spacer(
-                        flex: 2,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Nome do Evento",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                      ),
-                      SizedBox(
-                        height: 50,
-                        width: 5,
-                        child: VerticalDivider(
-                          color: Cores.cinzaEscuro,
-                          thickness: 2,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Comunidade:",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                      ),
-                      Spacer(flex: 2),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Nome da Comunidade",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                      ),
-                      SizedBox(
-                        height: 50,
-                        width: 5,
-                        child: VerticalDivider(
-                          color: Cores.cinzaEscuro,
-                          thickness: 2,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Data:",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                      ),
-                      Spacer(flex: 1),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("00/00/0000",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        const Text("Valor P/ Pessoa:",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: SizedBox(
-                            height: 45,
-                            child: CupertinoTextField(
-                              keyboardType: TextInputType.number,
-                              placeholder: "R\$ 0,00",
-                              placeholderStyle: const TextStyle(
-                                fontSize: 18,
-                              ),
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Cores.cinzaEscuro,
-                                  width: 1,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      height: 200,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                "Comunidade: Nome da Comunidade",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                borderRadius: BorderRadius.circular(5),
                               ),
                             ),
-                          ),
-                        ),
-                        // SizedBox(width: 10),
-                        // const Text("x 80", style: TextStyle(fontSize: 18)),
-                        const Spacer(),
-                        const Text(
-                          "Valor Total:",
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: SizedBox(
-                            height: 45,
-                            child: CupertinoTextField(
-                              keyboardType: TextInputType.number,
-                              placeholder: "R\$ 0,00",
-                              placeholderStyle: const TextStyle(
-                                fontSize: 18,
-                              ),
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Cores.cinzaEscuro,
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(5),
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: 10,
+                                itemBuilder: (context, index) {
+                                  return const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      vertical: 5,
+                                      horizontal: 10,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Text("Nome da Pessoa",
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold)),
+                                        Spacer(),
+                                        Text("R\$ 0,00",
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold)),
+                                      ],
+                                    ),
+                                  );
+                                },
                               ),
                             ),
-                          ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                   const Spacer(),
                   const Padding(
-                    padding: EdgeInsets.all(8.0),
+                    padding: EdgeInsets.all(10),
                     child: Row(
                       children: [
                         Text("Total:",
