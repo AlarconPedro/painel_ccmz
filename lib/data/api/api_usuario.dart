778406446fc6cd48db913363e0956132d3e9dd0e
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2024-05-26 12:37:28 -0300

    Adicionado rota de busca de usuários cadastrados - 2024.5.26.1

--- lib/data/api/api_usuario.dart
+++ lib/data/api/api_usuario.dart
@@ -6,12 +6,19 @@ import '../models/usuario_model.dart';
 class ApiUsuario {
   static final _request = HttpRequest();
 
+  //GET
   final String _urlLoginSistema = '${Globais.urlBase}Usuario/';
+  final String _urlBuscarUsuarios = '${Globais.urlBase}Usuario';
 
+  //POST
   final String _urlGravarUsuario = '${Globais.urlBase}Usuario';
 
   Future<dynamic> loginSistema(String codigoFirebase) async {
     return await _request.getJson("$_urlLoginSistema$codigoFirebase");
+  }
+
+  Future<dynamic> getUsuarios() async {
+    return await _request.getJson(_urlBuscarUsuarios);
   }
 
   Future<dynamic> gravarUsuario(UsuarioModel usuario) async {
