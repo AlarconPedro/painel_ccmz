d54a9b80ca52a44759ed981ecdbcda67c910ba7c
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2024-08-06 17:35:05 -0300

    Criado sorteio - 2024.8.6.5

--- lib/pages/admin/sorteios/sortear.dart
+++ lib/pages/admin/sorteios/sortear.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+import '../../../classes/classes.dart';
 
 class Sortear extends StatefulWidget {
   const Sortear({super.key});
@@ -8,8 +11,47 @@ class Sortear extends StatefulWidget {
 }
 
 class _SortearState extends State<Sortear> {
+  String texto = 'Sorteando...';
+
+  sortear() {}
+
+  @override
+  initState() {
+    super.initState();
+    sortear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          decoration: BoxDecoration(
+            color: Cores.verdeMedio,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  texto,
+                  style: const TextStyle(
+                    color: Cores.branco,
+                    fontSize: 20,
+                  ),
+                ),
+                LoadingAnimationWidget.inkDrop(
+                  color: Cores.branco,
+                  size: 200,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
