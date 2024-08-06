b5daece8577233c8d6b4b367d5d8265657abfbbb
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2024-08-06 17:12:56 -0300

    Criado base de sorteios e listagem dos sorteios - 2024.6.8.3

---
+++ lib/pages/admin/sorteios/sortear.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Sortear extends StatefulWidget {
+  const Sortear({super.key});
+
+  @override
+  State<Sortear> createState() => _SortearState();
+}
+
+class _SortearState extends State<Sortear> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
